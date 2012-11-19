1f5cead59c43511e5ebe9fafc3a929139b0d8a94
Julian <julianh2o@gmail.com>
2012-11-19 02:07:33 -0800

    organized todo list

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -8,5 +8,5 @@ if [ "$HOSTNAME" == "ares" ]; then
     git pull
     $PLAY start --%prod
 else
-    ssh root@new.podbase.net /home/julian/podbase/refresh.sh
+    ssh root@new.podbase.net /home/julian/podbase/bin/refresh.sh
 fi
