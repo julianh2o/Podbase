f4fac02332a8f681ce9ed80775997184162b3fe0
Julian <julianh2o@gmail.com>
2013-03-03 23:52:22 -0800

    adding search features

---
+++ app/controllers/SearchController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+import play.mvc.Util;
+import play.mvc.With;
+import services.PathService;
+import services.SearchService;
+
+import models.DatabaseImage;
+import models.Project;
+import models.Template;
+import models.TemplateAssignment;
+import models.TemplateAttribute;
+
+@With(Security.class)
+public class SearchController extends ParentController {
+    public static void index(String query) {
+    	Set<DatabaseImage> results = SearchService.performSimpleSearch(query);
+    	render(results);
+    }
+}
---
+++ app/services/SearchService.java
@@ -0,0 +1,45 @@
+package services;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import notifiers.Email;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+import controllers.ParentController;
+
+import access.AccessType;
+
+import play.mvc.Util;
+
+import models.Activation;
+import models.DatabaseImage;
+import models.Directory;
+import models.ImageAttribute;
+import models.ImageSetMembership;
+import models.Paper;
+import models.Permission;
+import models.PermissionedModel;
+import models.Project;
+import models.User;
+
+public class SearchService {
+	public static Set<DatabaseImage> performSimpleSearch(String query) {
+		Set<DatabaseImage> results = new HashSet<DatabaseImage>();
+		
+		List<ImageAttribute> found = ImageAttribute.find("byValue", query).fetch();
+		for (ImageAttribute attr : found) {
+			results.add(attr.image);
+		}
+		
+		return results;
+	}
+
+}
