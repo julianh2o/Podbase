fcb04ec2948b7d242ad6a38a48ed91b8306c6c3d
Julian <julianh2o@gmail.com>
2013-05-29 21:15:50 -0700

    improving setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,4 +3,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PODBASE_DIR="$(dirname "$SCRIPT_DIR")"
 
-cp $PODBASE_DIR/conf/application.conf.example $PODBASE_DIR/conf/application.conf
+SRC=$PODBASE_DIR/conf/application.conf.example
+DST=$PODBASE_DIR/conf/application.conf
+
+if [ -a "$SRC" ]
+then
+    echo "Configuration already exsts: $DST"
+else
+    cp $SRC $DST
+    echo "Created configuration file: $DST"
+fi
