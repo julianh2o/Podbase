10e52c48baf8f9836e0bcaaaebc04da90f92c314
Julian <julianh2o@gmail.com>
2013-05-29 17:19:52 -0700

    updating refresh

--- bin/podbase2.sh
+++ bin/podbase2.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-PLAY=/home/imgup/play/play
-HOSTNAME=`hostname`
-if [ "$HOSTNAME" == "podbase2" ]; then
-    cd /home/imgup/podbase
-    $PLAY stop
-    rm server.pid
-    git pull
-    $PLAY dependencies
-    $PLAY start --%prod
-else
-    ssh julian@podbase2.pbrc.hawaii.edu "sudo /home/julian/podbase/bin/refresh.sh"
-fi
+ssh -t julian@podbase2.pbrc.hawaii.edu "sudo /home/julian/podbase/bin/refresh.sh"
