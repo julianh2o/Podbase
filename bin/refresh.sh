80bc3028025a003ee85715050b545c9bbfe45006
root <root@podbase2.pbrc.hawaii.edu>
2013-06-30 19:38:16 -1000

    fixing refresh to not interrupt

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -4,9 +4,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PODBASE_DIR="$(dirname "$SCRIPT_DIR")"
 PLAY=/home/imgup/play/play
 
+echo "Using: $PODBASE_DIR"
 cd $PODBASE_DIR
+pwd
 $PLAY stop
 rm server.pid
 git pull
 $PLAY dependencies
-$PLAY start --%prod
+nohup $PLAY start --%prod
+#tail -f logs/system.out logs/application.log
