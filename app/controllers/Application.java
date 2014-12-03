872151dfb88b622a6af2efdb0e21f0d66ac9c865
Julian Hartline <julianh2o@gmail.com>
2014-12-02 17:27:46 -0800

    implemented some clean up scripts fixMissingFiles and fixDuplicateImages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,6 +11,8 @@ import play.modules.search.Search;
 import play.modules.search.store.FilesystemStore;
 import play.mvc.*;
 import services.PathService;
+import util.Stopwatch;
+import util.TaskCompletionEstimator;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,7 +20,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.Map.Entry;
@@ -29,6 +36,7 @@ import access.ModelAccess;
 
 import javax.persistence.Query;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.ivy.util.FileUtil;
 
@@ -70,7 +78,140 @@ public class Application extends ParentController {
     	jsonOk();
     }
     
-    @ModelAccess(AccessType.LISTED)
+    public static void fixDuplicateImageEntries() {
+    	User user = Security.getUser();
+    	if (!user.isRoot()) forbidden();
+    	List<DatabaseImage> images = DatabaseImage.find("SELECT img FROM DatabaseImage img WHERE (SELECT COUNT(imgs) FROM DatabaseImage imgs WHERE imgs.path=img.path) > 1").fetch();
+    	for (DatabaseImage img : images) {
+    		List<DatabaseImage> duplicates = DatabaseImage.find("byPath", img.getStringPath()).fetch();
+    		List<ImageAttribute> attributes = new LinkedList<ImageAttribute>();
+    		DatabaseImage first = null;
+    		for (DatabaseImage dup : duplicates) {
+    			if (first == null) {
+    				first = dup;
+    			} else {
+	    			for (ImageAttribute attr : dup.attributes) {
+	    				attributes.add(attr);
+	    				attr.image = dup;
+	    				attr.save();
+	    			}
+	    			dup.delete();
+    			}
+    		}
+    	}
+    	renderText(images.size()+" images with duplicates cleaned up.");
+    }
+    
+    public static void fixMissingFiles() throws IOException {
+    	User user = Security.getUser();
+    	if (!user.isRoot()) forbidden();
+        Runtime runtime = Runtime.getRuntime();
+        
+        int mb = 1024*1024;
+
+    	System.out.println("loading images...");
+    	List<DatabaseImage> images = DatabaseImage.all().fetch();
+    	System.out.println("loaded "+images.size()+" images.");
+    	
+    	System.out.println("loading pvis");
+    	List<ProjectVisibleImage> pvis = ProjectVisibleImage.all().fetch();
+    	System.out.println("loaded "+pvis.size()+" pvis");
+    	
+    	HashMap<Long,ProjectVisibleImage> byImageId = new HashMap<>();
+    	for (ProjectVisibleImage pvi : pvis) {
+    		byImageId.put(pvi.image.id, pvi);
+    	}
+    	System.out.println("finished processing pvis");
+    	
+    	int deleted = 0;
+    	int directories = 0;
+    	StringBuilder sb = new StringBuilder();
+    	TaskCompletionEstimator est = new TaskCompletionEstimator(10,10,images.size());
+    	System.out.println("loading paths..");
+    	HashSet<String> existingPaths = getAllFiles(PathService.getRootImageDirectory());
+    	System.out.println("found "+existingPaths.size()+" files");
+		long lastLog = System.currentTimeMillis();
+		Stopwatch sw = new Stopwatch();
+		List<Long> pviDeleteList = new LinkedList<>();
+		List<Long> imageDeleteList = new LinkedList<>();
+    	for (DatabaseImage img : images) {
+    		sw.start("all");
+    		est.tick();
+    		boolean pathExists = existingPaths.contains(img.getStringPath());
+    		if (!pathExists) {
+    			boolean isDirectory = img.getPath().toFile().isDirectory();
+    			if (isDirectory) {
+    				directories++;
+    			} else if (img.attributes.size() == 0) {
+    				if (byImageId.containsKey(img.id)) {
+    					pviDeleteList.add(byImageId.get(img.id).id);
+    				}
+					imageDeleteList.add(img.id);
+	    			deleted++;
+	    		} else {
+	    			sb.append(img.getStringPath()+" does not exist, but has "+img.attributes.size()+" attributes.\n");
+	    		}
+	    		
+		        if (System.currentTimeMillis() - lastLog > 10000) {
+					lastLog = System.currentTimeMillis();
+		        	System.out.println(est.getStatusLine());
+			        System.out.println("Used Memory:" + ((runtime.totalMemory() - runtime.freeMemory()) / mb) + " of "+(runtime.totalMemory()/mb)+" MB");
+		        	//System.out.println(sw.toString());
+		        }
+    		}
+    		sw.stop("all");
+    	}
+    	
+    	StringBuilder sql = new StringBuilder();
+    	int index = 0;
+    	for (Long id : pviDeleteList) {
+    		if (index == 0) 
+    			sql.append("DELETE FROM ProjectVisibleImage WHERE id IN (");
+    		else
+    			sql.append(",");
+    		sql.append(id);
+    		index++;
+    		if (index % 100 == 0) {
+    			sql.append(");\n");
+    			index = 0;
+    		}
+    	}
+    	if (index != 0) sql.append(");\n");
+    	
+    	for (Long id : imageDeleteList) {
+    		if (index == 0) 
+    			sql.append("DELETE FROM DatabaseImage WHERE id IN (");
+    		else
+    			sql.append(",");
+    		sql.append(id);
+    		index++;
+    		if (index % 100 == 0) {
+    			sql.append(");\n");
+    			index = 0;
+    		}
+    	}
+    	if (index != 0) sql.append(");\n");
+    	
+    	File f = PathService.getApplicationPath().resolve("fixMissingFiles.sql").toFile();
+    	System.out.println("writing to: "+f.getAbsolutePath());
+    	FileUtils.writeStringToFile(f, sql.toString());
+    	
+    	renderText(sb.toString()+"\nWriting to fixMissingFiles.sql\n"+deleted+" nonexistent and un-attributed images deleted. "+directories+" directories were not deleted.");
+    }
+    
+    private static HashSet<String> getAllFiles(Path root) throws IOException {
+    	final HashSet<String> paths = new HashSet<>();
+    	Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+		    @Override
+		    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		    	paths.add(PathService.getRelativeString(file));
+		        return FileVisitResult.CONTINUE;
+		    }
+    	});
+		return paths;
+	}
+
+	@ModelAccess(AccessType.LISTED)
     public static void entry(Long projectId) {
     	Project project = Project.findById(projectId);
     	if (project == null) error("Project not found!");
