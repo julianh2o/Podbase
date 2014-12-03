65ea757d370c5d5f06430602d1ac7e0a3025e299
Julian Hartline <julianh2o@gmail.com>
2014-12-03 15:00:12 -0800

    adding root requirement to job manager

--- app/controllers/JobManagerController.java
+++ app/controllers/JobManagerController.java
@@ -14,16 +14,19 @@ import services.JobManager.JobContainer;
 @With(Security.class)
 public class JobManagerController extends ParentController {
 	public static void index() {
+		if (!Security.getUser().isRoot()) forbidden("Must be root");
 		List<JobContainer> jobs = JobManager.getInstance().getJobs();
 		render(jobs);
 	}
 	
 	public static void startJob(int index) {
+		if (!Security.getUser().isRoot()) forbidden("Must be root");
 		JobManager.getInstance().startJob(index);
 		index();
 	}
 	
 	public static void stopJob(int index) {
+		if (!Security.getUser().isRoot()) forbidden("Must be root");
 		JobManager.getInstance().stopJob(index);
 		index();
 	}
